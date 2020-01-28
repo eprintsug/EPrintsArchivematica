dcb7df6dfa6b7b87547585f7b7cbd99cbe1a7690
Will Fyson <williamfyson89@hotmail.com>
2020-01-28 13:33:31 +0000

    Trigger now creates archivematica records when elected eprint fields values change, process_transfers exports the data for those records where is_dirty=1

--- cfg/cfg.d/z_archivematica.pl
+++ cfg/cfg.d/z_archivematica.pl
@@ -26,18 +26,64 @@ push @{$c->{user_roles}->{admin}}, qw{ +archivematica/view +archivematica/destro
 $c->{archivematica}->{path} = $c->{archiveroot}.'/archivematica';
 
 # Include Derivatives?
-$c->{DPExport}->{include_derivatives} = 0;
+$c->{DPExport}->{include_derivatives} = 1;
+
+# example fields to trigger record creation
+$c->{DPExport}->{trigger_fields}->{meta_fields} = [ qw/ title creators_name creators_id fileinfo / ]; 
 
 $c->add_dataset_trigger( 'eprint', EPrints::Const::EP_TRIGGER_AFTER_COMMIT, sub
 {
 	my( %args ) = @_;
-	my( $repo, $eprint, $changed ) = @args{qw( repository dataobj changed )};
+	my( $session, $eprint, $changed ) = @args{qw( repository dataobj changed )};
 
  	## To Do ##
  	# Establish what has changed... and decide if we need to process a new Archivematica
  	# transfer... create a new Archivematica record if this EPrint doesn't already have 
  	# one, or update an existing one if it does.
- 	
+
+	my $action_required = 0;
+	foreach my $f ( @{ $c->{DPExport}->{trigger_fields}->{meta_fields} } )
+	{
+		if( defined $changed->{ $f } )
+		{
+			print STDERR "New Archivematica transfer required due to field '$f' changes\n";
+			$action_required = 1;
+    			# last;
+  		}
+	}
+
+	if( $action_required )
+	{
+		# create an archivematica record for this item if one doesnt exist
+		# the process_transfers offline script can run over these and generate exports (and zip files)
+
+		my $ds = $session->dataset( "archivematica" );
+		my $searchexp = new EPrints::Search( session=>$session, dataset=>$ds );
+		$searchexp->add_field( $ds->get_field( "datasetid" ), "eprint", "EQ" );
+		$searchexp->add_field( $ds->get_field( "dataobjid" ), $eprint->id, "EQ" );
+		my $list = $searchexp->perform_search;
+
+		if( $list && $list->count() > 0 )
+		{
+			# take the first result and set is_dirty if its not already set
+			my $a = $list->item(0);
+			if( $a->get_value( "is_dirty" ) == 0 )
+			{
+				$a->set_value( "is_dirty", 1 );
+				$a->commit();
+			}
+		}
+		else
+		{
+			# create a new entry
+			$session->dataset( "archivematica" )->create_dataobj({
+				datasetid => "eprint",
+				dataobjid => $eprint->id,
+				is_dirty => 1,
+			});
+		}
+	}
+
  	return EP_TRIGGER_OK;
 });
 
