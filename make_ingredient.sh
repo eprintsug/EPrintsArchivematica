b9ada1975d83f05137b880b64fa363962770f225
Justin Bradley <jb4@ecs.soton.ac.uk>
2021-05-26 10:57:08 +0000

    Script for making an EPrints 3.4 ingredient from these files

---
+++ make_ingredient.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Copy the contents into the structure used by an EPrints 3.4 ingredient.
+
+# run ./make_ingredient.sh
+# then place ingredients/archivematica under your EPrints 3.4 ingredients directory
+# update your flavours/pub_lib/inc file to include ingredients/archivematica
+# run epadmin update REPO
+# apachectl graceful
+
+I=ingredients/archivematica
+mkdir -p $I
+
+cp -r bin $I
+cp -r cgi $I
+
+cp -r cfg/cfg.d $I
+# cp -r cfg/citations $I # unused?
+
+cp -r lib/citations $I
+cp -r lib/lang $I
+cp -r lib/plugins $I
+cp -r lib/workflows $I
